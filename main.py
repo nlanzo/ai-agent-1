73eb852fe9b43413decbdfb812eaa86e8054a925
Nick Lanzo <nlanzo@gmail.com>
2025-06-12 21:03:23 -0500

    setup client to send a message and receive a response

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+contents = sys.argv[1]
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=contents)
+
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
