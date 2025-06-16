304cd27abc8e0878f589392ed6e82785740e8479
Nick Lanzo <nlanzo@gmail.com>
2025-06-16 16:15:34 -0500

    add ability for agent to CALL functions, rather than just choose which function is appropriate

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,8 +1,9 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import schema_write_file, write_file
+from config import WORKING_DIRECTORY
 
 
 available_functions = types.Tool(
@@ -13,3 +14,46 @@ available_functions = types.Tool(
         schema_write_file
     ]
 )
+
+
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    args = function_call_part.args
+    
+    if verbose:
+        print(f"Calling function: {function_name}({args})")
+    else:
+        print(f" - Calling function: {function_name}")
+
+    kwargs = args
+    kwargs["working_directory"] = WORKING_DIRECTORY
+    
+    functions = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+
+    if function_name in functions:
+        function_result = functions[function_name](**kwargs)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 from prompts import system_prompt
 
 def main():
@@ -41,9 +41,19 @@ def generate_content(client, messages, verbose):
     if not response.function_calls:
         return response.text
 
+    function_responses = []
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        if not function_call_result.parts[0].function_response.response:
+             raise Exception("no response from function")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 
+        function_responses.append(function_call_result.parts[0])
 
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+    
+    
 if __name__ == "__main__":
     main()
