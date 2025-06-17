24f0df985253aafa47b2245a346534ffa1919298
Nick Lanzo <nlanzo@gmail.com>
2025-06-17 09:05:42 -0500

    update run_python_file tool to accept arguments

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -3,7 +3,7 @@ import subprocess
 from google.genai import types
 
 
-def run_python_file(working_directory, file_path):
+def run_python_file(working_directory, file_path, args=None):
     absolute_directory = os.path.abspath(os.path.join(working_directory, file_path))
     absolute_working_directory = os.path.abspath(working_directory)
 
@@ -17,7 +17,10 @@ def run_python_file(working_directory, file_path):
         return f'Error: "{file_path}" is not a Python file'
     
     try:
-        result = subprocess.run(['python', absolute_directory], capture_output=True, text=True, timeout=30, check=True)
+        commands = ['python', absolute_directory]
+        if args:
+            commands.extend(args)
+        result = subprocess.run(commands, capture_output=True, text=True, timeout=30, check=True, cwd=absolute_working_directory)
         stdout = result.stdout
         stderr = result.stderr
         return f'Successfully executed "{file_path}"\n\nSTDOUT:\n{stdout}\n\nSTDERR:\n{stderr}'
@@ -37,8 +40,17 @@ schema_run_python_file = types.FunctionDeclaration(
         properties={
             "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The path of the python file to run, relative to the working directory.",
+                description="Path to the Python file to execute, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional arguments to pass to the Python file.",
+                ),
+                description="Optional arguments to pass to the Python file.",
             ),
         },
+        required=["file_path"],
     ),
 )
